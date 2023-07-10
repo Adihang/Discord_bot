a481195d24849ae50e40dc1d723ee9392828d265
Adihang <limhan456@naver.com>
2023-07-10 10:54:11 +0900

    massage

--- MessegeDef.py
+++ MessegeDef.py
@@ -9,7 +9,7 @@ class MessageDef:
             todayQuiz = "오늘은 휴일입니다.\n코딩에는 체력도 중요하니 밖에 나가서 운동을 해보는건 어떨까요?"
         else:
             SqlConnect = SQLConnect()
-            todayQuiz = "# 오늘의 문제입니다!\n- 사용 언어는 **JavaScript** 또는 **Python**입니다.\n- 문제를 해결하고 자신의 코드를 '이름_문제이름'로 저장해 PR 해보세요!\n"
+            todayQuiz = "# 오늘의 문제입니다!\n- 사용 언어는 **JavaScript** 또는 **Python**입니다.\n- 문제를 해결하고 자신의 코드를 '이름_문제이름'로 저장해 PR 해보세요!\n\n- 만약 난이도를 더 높여도 될 것 같다 생각하시면 :arrow_up:\n- 적당하면 :thumbsup:\n- 너무 어려운것 같다 생각하시면 :arrow_down: 을 눌러주세요.\n"
             today = str(datetime.now().strftime("%Y%m%d"))
             today_quiz_DB = SqlConnect.get_day_quiz(today)
             for i in today_quiz_DB:
